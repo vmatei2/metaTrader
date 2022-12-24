55749b2298f412e4121d3dccb2b0e090595b6887
Vlad Matei <vlad.mateei@gmail.com>
2022-12-24 18:20:18 +0000

    saving retrieved article dict and loading it using pickle

--- data_retrieval/gdelt_retrieval.py
+++ data_retrieval/gdelt_retrieval.py
@@ -1,4 +1,7 @@
 import datetime
+import json
+import pickle
+
 from gdeltdoc import GdeltDoc, Filters
 
 
@@ -17,6 +20,8 @@ def query_gdelt(date_list):
     article_dict = {}
     timeine_dict = {}
     for i in range(len(date_list)):
+        # each entry in the data frame will contain news for a week - split up in order to avoid limitations of the
+        # gdelt api (250 returns)
         if i % 7 == 0:
             start_date = str(date_list[i])
             end_date = str(date_list[i + 6])
@@ -27,10 +32,24 @@ def query_gdelt(date_list):
                 country=["UK", "US"]
             )
             article_dict[start_date] = gd.article_search(filters=f)
+           # timeine_dict[date_list[i]] = gd.timeline_search("timelinetone", filters=f)
+        save_dict("article_dict.txt", article_dict)
     return article_dict
 
+
+def save_dict(filepath, dict_to_save):
+    with open(filepath, 'wb') as f:
+        pickle.dump(dict_to_save, f)
+    return dict_to_save
+
+def load_dict(filepath):
+    with open(filepath, 'rb') as pickle_file:
+        article_dict = pickle.load(pickle_file)
+    return article_dict
 
 start_date = datetime.date(2022, 1, 1)
 end_date = datetime.date(2022, 10, 1)
 date_list = generate_spaced_entries(start_date, end_date)
-article_dict = query_gdelt(date_list)
+# article_dict = query_gdelt(date_list)
+article_dict = load_dict("article_dict.txt")
+test = 0
